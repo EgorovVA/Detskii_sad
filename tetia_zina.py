9aa7e5e6ff9a527245f809486eff1e1739014f35
EgorovVA <83126642+EgorovVA@users.noreply.github.com>
2023-03-05 18:06:46 +0300

    test1

--- tetia_zina.py
+++ tetia_zina.py
@@ -69,17 +69,13 @@ class vospitatel(bascet,mom,analitic):
             char = load[0][0]
         self.data[char]+=[load+vedro.down()]
 
-    def finde_el(self, arr,load):
-        for i in range(len(arr)):
-            print(len(arr))
-            if(arr[i][0].lower()==load[0].lower() and arr[i][1]==load[1] and arr[i][2]==load[2]):
-                
+    def finde_el(self, arr,load, len):
+        for i in range(len):
+            if(arr[i][0]==load[0] and arr[i][1]==load[1] and arr[i][2]==load[2]):
                 return i
         return -1
     
     def del_child(self,load):
-
-
         self.app_del_gender_age(load, -1)
 
         if('а'<=load[0][0] and 'я'>=load[0][0]):
@@ -88,9 +84,9 @@ class vospitatel(bascet,mom,analitic):
             char = load[0][0]
 
         arr = self.data[char]
-        print("!!!!!!!",len(arr))
-        index = self.finde_el(arr,load)
-        if index !=0:
+
+        index = self.finde_el(arr,load,len(arr))
+        if index !=-1:
             print("Забираем игрушки: "+str(arr[index][3])+ " " +str(arr[index][4])+str(arr[index][5]) )
             arr.pop(index)
         else:
@@ -108,16 +104,7 @@ if __name__ == '__main__':
     
 
 
-    m = db_info['мальчики']
-    w = db_info['девочки']
-    age = db_info['возвраст']
 
-
-
-    a = vospitatel(db, db_info, age, m, w)
-    cout_arr_age = []
-    for i in range(8):
-        cout_arr_age.append(i)
     
     start =  int(input("Создать новый файл или работать с ныненшним(0/1): "))
     if(start == 0):
@@ -131,7 +118,13 @@ if __name__ == '__main__':
         db_info['мальчики'] =0
         db_info['девочки'] = 0
         db_info['возвраст'] =age
-
+    m = db_info['мальчики']
+    w = db_info['девочки']
+    age = db_info['возвраст']
+    a = vospitatel(db, db_info, age, m, w)
+    cout_arr_age = []
+    for i in range(8):
+        cout_arr_age.append(i)
     while(1):
 
         menu = int(input("Здраствуйте, что вы хотите сделать (напишите число из пунктов):\n 1)Привести детей\n 2)Время уже 12:00, надо узнать сколько мальчиков и девочек в садике\n 3)Время уже 12:00, надо узнать колличсетыо детей разного возвраста\n 4)Забрать детей\n 5)Вывести все данные о детях в садике\n 6)Сохранить и выйти \n"))
@@ -164,18 +157,10 @@ if __name__ == '__main__':
             reg_gata = child(name,int(age),gender)
             load = reg_gata.unslall_child()
             a.del_child(load)
-            db.close()
-            db= shelve.open("Дети")
+
         if(menu==5):
             a.write_all_data(db)
             
         if(menu==6):
             db.close()
             raise SystemExit
-           
-        
-
-
-    
-
-
